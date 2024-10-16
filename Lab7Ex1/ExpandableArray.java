027e78421cfeb47b129fa9abc18f1ceb392ee629
Uyen Tran <tranphuonguyen1011@gmail.com>
2024-10-16 17:11:48 +0100

    Add array examples

---
+++ Lab7Ex1/ExpandableArray.java
@@ -0,0 +1,71 @@
+package Lab7Ex1;
+
+public class ExpandableArray 
+{
+    private int[] myArray = new int[10]; // Initial size
+    private int pos = 0; // Next position to add element into   
+    
+    public void add(int a)
+    {
+        if (pos >= myArray.length)
+        {
+            // Make array bigger
+            int currentLength = myArray.length;
+            int newLength = currentLength + 10;
+            int[] newArray = new int[newLength];
+
+            // Copy elements to new array
+            System.arraycopy(myArray, 0, newArray, 0, currentLength);
+            
+            // Reset the reference
+            myArray = newArray;
+        }
+
+        myArray[pos++] = a;
+    }
+
+    public int get(int index)
+    {
+        if (index < 0 || index > pos)
+        {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        else 
+        {
+            return myArray[index];
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+        String line = "";
+        for (int i = 0; i < pos; i++)
+        {
+            line += myArray[i] + " ";
+        }
+        return line;
+    }
+
+    public int getLength()
+    {
+        return myArray.length;
+    }
+
+    public static void main(String[] args) 
+    {
+        ExpandableArray arr = new ExpandableArray();
+        for (int i = 0; i < 10; i++)
+        {
+            arr.add(i);
+        }
+
+        System.out.println(arr);
+        System.out.println(arr.getLength());
+        arr.add(10);
+        System.out.println(arr);
+        System.out.println(arr.getLength());
+        System.out.println(arr.get(9));
+        
+    }
+}
---
+++ Lab7Ex1/IntArray.java
@@ -0,0 +1,14 @@
+package Lab7Ex1;
+
+public class IntArray 
+{
+    public static void main (String[] args)
+    {
+        int[] myIntArray = new int[6];
+
+        myIntArray[0] = 1;
+        myIntArray[1] = 2;
+
+        System.err.println(myIntArray[5]);
+    }
+}
