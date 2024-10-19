3a3441505c5a65ee3053e3eab667275ba145e41f
Uyen Tran <tranphuonguyen1011@gmail.com>
2024-10-19 10:38:13 +0100

    Close scanners in Lab3

--- Lab3Ex1/Lab3Ex3.java
+++ Lab3Ex1/Lab3Ex3.java
@@ -15,63 +15,60 @@ public class Lab3Ex3 {
       
    // Promt for user's first name, last name age and print them
    public static void printNameAndAge() {
-      Scanner s = new Scanner(System.in);
-      System.out.println("Enter your first name, last name and your age with a space in between, then press return.");
-      String nameAndAge = s.nextLine();
-      // s.close();
-      
-      Scanner t = new Scanner(nameAndAge);
-      String firstName = t.next();
-      String lastName = t.next();
-      int age = t.nextInt();
-      // t.close();
-      
-      System.out.println("First name: " + firstName);
-      System.out.println("Last name: " + lastName);
-      System.out.println("Age: " + age);
+      try (Scanner s = new Scanner(System.in)) {
+         System.out.println("Enter your first name, last name and your age with a space in between, then press return.");
+         String nameAndAge = s.nextLine();
+         
+         try (Scanner t = new Scanner(nameAndAge)) {
+            String firstName = t.next();
+            String lastName = t.next();
+            int age = t.nextInt();
+            
+            System.out.println("First name: " + firstName);
+            System.out.println("Last name: " + lastName);
+            System.out.println("Age: " + age);
+         }
+      }
    }
          
    // Promt for user's date of birth and return date of birth as a string
    public static String getDateofBirth() {
-      Scanner s = new Scanner(System.in);
-      System.out.println("Enter your date of birth in the format: DD MM YYYY.");
-      String dateOfBirth = s.nextLine();
-      System.out.println("Date of birth: " + dateOfBirth);
-      // s.close();
-      return dateOfBirth;
+      try (Scanner s = new Scanner(System.in)) {
+         System.out.println("Enter your date of birth in the format: DD MM YYYY.");
+         String dateOfBirth = s.nextLine();
+         System.out.println("Date of birth: " + dateOfBirth);
+         return dateOfBirth;
+      }
    }
          
    // Compute user's age based on date of birth
    public static int computeAge(String dateOfBirth) {
-      Scanner u = new Scanner(dateOfBirth);
-      int birthDay = u.nextInt();
-      int birthMonth = u.nextInt();
-      int birthYear = u.nextInt();
-      // u.close();
-      
-      // Promt for today's date
-      Scanner v = new Scanner(System.in);
-      System.out.println("Enter your today's date in the format: DD MM YYYY.");
-      
-      int thisDay = v.nextInt();
-      int thisMonth = v.nextInt();
-      int thisYear = v.nextInt();
-      
-      // v.close();
+      try (Scanner u = new Scanner(dateOfBirth)) {
+         int birthDay = u.nextInt();
+         int birthMonth = u.nextInt();
+         int birthYear = u.nextInt();
+         
+         int userAge;
+          try ( // Promt for today's date
+                  Scanner v = new Scanner(System.in)) {
+              System.out.println("Enter your today's date in the format: DD MM YYYY.");
+              int thisDay = v.nextInt();
+              int thisMonth = v.nextInt();
+              int thisYear = v.nextInt();
+              // Compute user's age
+              userAge = thisYear - birthYear;
+              if (thisMonth < birthMonth) {
+                  userAge--;
+              }
+              else if (thisMonth == birthMonth) {
+                  if (thisDay < birthDay) {
+                      userAge--;
+                  }
+              }    u.close();
+          }
 
-      // Compute user's age
-      int userAge = thisYear - birthYear;
-      
-      if (thisMonth < birthMonth) {
-         userAge--;
+         return userAge;
       }
-      else if (thisMonth == birthMonth) {
-         if (thisDay < birthDay) {
-            userAge--;
-         }
-      }
-
-      return userAge;
    }
 }
 
--- Lab3Ex1/Lab3Ex4.java
+++ Lab3Ex1/Lab3Ex4.java
@@ -3,13 +3,14 @@ package Lab3Ex1;
 import java.util.Scanner;
 
 public class Lab3Ex4 {
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter your age: ");
-        int age = s.nextInt();
-        // s.nextLine();
-        System.out.println("You are " + age + " years old!");
-        System.out.println("Do you enjoy being so old?");
-        String answer = s.nextLine();
+        try (Scanner s = new Scanner(System.in)) {
+            System.out.println("Enter your age: ");
+            int age = s.nextInt();
+            System.out.println("You are " + age + " years old!");
+            System.out.println("Do you enjoy being so old?");
+            String answer = s.nextLine();
+        }
     }     
 }
